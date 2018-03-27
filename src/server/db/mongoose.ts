09017709f160e6b8a87e72546085e9bcbd757684
BernardoMB <bmondragonbrozon@gmail.com>
2018-03-26 21:12:13 -0600

    Added mlab for Mongo addon on heroku deployment

--- src/server/db/mongoose.ts
+++ src/server/db/mongoose.ts
@@ -6,8 +6,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // Connect to the database.
-//mongoose.connect(process.env.MONGODB_URI);
-mongoose.connect('mongodb://localhost:27017/herita');
+mongoose.connect(process.env.MONGODB_URI);
 
 // Export the connection.
 module.exports = {mongoose};
